b57a959c89b7b41d2df01c4a7d5aadb331794e09
DmitryLebedinsky <ldo172@mail.ru>
2022-06-26 14:50:42 +0300

    fix async loop

--- index.js
+++ index.js
@@ -1,58 +1,69 @@
 const axios = require("axios");
 const HTMLParser = require('node-html-parser');
 
-let url = "https://www.dissercat.com/search?q=";
+main();
 
-const query = '%D0%BC%D0%B5%D1%82%D0%BE%D0%B4%D0%B8%D0%BA%D0%B0+%D0%BF%D0%BB%D0%B0%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F'
-url = url + query
+async function main() {
+    let url = "https://www.dissercat.com/search?q=";
+    const query = '%D0%BC%D0%B5%D1%82%D0%BE%D0%B4%D0%B8%D0%BA%D0%B0+%D0%BF%D0%BB%D0%B0%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F'
+    const diserts = [];
+    let page = 1;
 
-const diserts = [];
+    for (let i = 1; i <= page; i++) {
+        let _url = url + query;
 
-let page = 1;
-while (page < 20){
+        if (i > 1) {
+            _url = _url + '&page=' + i;
+        }
 
-if (page != 1){
-    url = url + '&page=' + page;
+        const res = await axios.get(_url)
+
+        if (res?.data) {
+            const document = HTMLParser.parse(res.data);
+            const searchResults = document.querySelectorAll('.search-result');
+
+            searchResults.forEach((element) => {
+                const disert = {};
+                disert.title = element.querySelector('a').textContent;
+                disert.keyWords = element.querySelector('a').getElementsByTagName('strong').map(strongMap);
+                const infoArr = element.querySelector('h3 > span').innerHTML.split(' &mdash; ');
+                disert.year = infoArr[0];
+                disert.author = infoArr[1];
+
+                diserts.push(disert);
+            });
+
+            page = getLastPage(document);
+
+            await delay();
+        }
+    }
+    console.log(diserts);
 }
 
-    axios.get(url).then(resultHandler);
-    page++;
-}
-
- 
-
-function resultHandler(res) {
-    var document = HTMLParser.parse(res.data);
-    const searchResults = document.querySelectorAll('.search-result');
-    searchResults.forEach(handlerElement);
-    getLastPage(document);
-}
-
-function getLastPage(document){
+function getLastPage(document) {
     const pages = document.querySelectorAll('.page-item').map(pageMap).filter(pageFilter)
-    console.log(pages)
+    const lastPage = Math.max(...pages);
+
+    if (lastPage > 1) {
+        return lastPage;
+    }
+
+    return 1;
 }
 
-function pageMap(element){
+function pageMap(element) {
     return parseInt(element.textContent, 10)
 }
 
-function pageFilter(page){
+function pageFilter(page) {
     return !isNaN(page)
 }
 
-function handlerElement(element){
-    const disert = {};
-    disert.title = element.querySelector('a').textContent;
-    disert.keyWords = element.querySelector('a').getElementsByTagName('strong').map(strongMap);
-    const infoArr = element.querySelector('h3 > span').innerHTML.split(' &mdash; ');
-    disert.year = infoArr[0];
-    disert.author = infoArr[1];
-
-    diserts.push(disert);
-}
-
-function strongMap(strong){
+function strongMap(strong) {
     return strong.textContent;
 }
 
+function delay() {
+    return new Promise(resolve => setTimeout(resolve, 500));
+}
